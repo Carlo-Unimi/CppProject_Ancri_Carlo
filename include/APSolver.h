7f953b88491f598d24c92435720f22d950824242
carlo.ancri <carlo.ancri@studenti.unimi.it>
2025-06-17 15:02:45 +0200

    added throws tag in the comments of AP

--- include/APSolver.h
+++ include/APSolver.h
@@ -16,6 +16,8 @@ public:
      * - Once a task is assigned, it becomes unavailable for other agents
      * - It proceeds sequentially through the agents and does not backtrack
      * This approach does not guarantee optimality, but it provides a feasible assignment.
+     * 
+     * @throws std::runtime_error if it is not possible to assign all agents to tasks.
      */
     std::unique_ptr<ProblemSolution> solve(const ProblemInstance* instance) override;
 };
